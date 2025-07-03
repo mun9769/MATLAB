68cd710662aba87040609b8b3641d70e259a1052
Moon <mun9769@naver.com>
2025-07-03 23:42:00 +0900

    update

--- book_mr_bae/chapter03_control_MTPA_plot.m
+++ book_mr_bae/chapter03_control_MTPA_plot.m
@@ -31,65 +31,91 @@ Ismax = 10; % Arms
 eqn = (Vsmax/w)^2 == (Lamf+Lds*ids)^2 + (Lqs*iqs)^2;
 wrpm_val = [1000 1200 1400 1600 1800];
 wr_val = wrpm_val*(2*pi/60) * (P/2);  
-w_plot1=subs(eqn, w, wr_val(3)); 
-w_plot2=subs(eqn, w, wr_val(4)); 
-w_plot3=subs(eqn, w, wr_val(5)); 
+w_eqn1=subs(eqn, w, wr_val(3));
+w_eqn2=subs(eqn, w, wr_val(4));
+w_eqn3=subs(eqn, w, wr_val(5));
+w_solution = solve(eqn, w);
 
 % 전류 제한원
 eqn = Te == 3/2*P * (ids*iqs*(Lds - Lqs) + Lamf*iqs);
-Te_plot1 = subs(eqn, Te, 10);
-Te_plot2 = subs(eqn, Te, 20);
-Te_plot3 = subs(eqn, Te, 30);
+Te_eqn1 = subs(eqn, Te, 10);
+Te_eqn2 = subs(eqn, Te, 20);
+Te_eqn3 = subs(eqn, Te, 30);
+Te_eqn4 = subs(eqn, Te, 40);
 % Lagrange 승수법을 사용해서 곡선과 원점 사이의 최단거리를 구한다.
-[~, ~, dist_1] = my_Lagrange_multiplier(eDiff(Te_plot1), ids, iqs);
-[~, ~, dist_2] = my_Lagrange_multiplier(eDiff(Te_plot2), ids, iqs);
-[~, ~, dist_3] = my_Lagrange_multiplier(eDiff(Te_plot3), ids, iqs);
+[~, ~, dist_1] = my_Lagrange_multiplier(eDiff(Te_eqn1), ids, iqs);
+[~, ~, dist_2] = my_Lagrange_multiplier(eDiff(Te_eqn2), ids, iqs);
+[~, ~, dist_3] = my_Lagrange_multiplier(eDiff(Te_eqn3), ids, iqs);
+[~, ~, dist_4] = my_Lagrange_multiplier(eDiff(Te_eqn4), ids, iqs);
+circle_eqn1 = ids^2+iqs^2 == dist_4^2;
+
+
+% 전압제한원과 전류제한원의 교점 구하기.
+[sol_ids, sol_iqs] = solve([w_eqn3 circle_eqn1], [ids iqs]);
+sol_ids = double(sol_ids);
+sol_iqs = double(sol_iqs);
+sol_ids = sol_ids(1);
+sol_iqs = sol_iqs(1);
+
+
 
 
 if ~exist('MTPA_position', 'var')
-    for te=1:100
-        eqn_plot = subs(eqn, Te, te);
-
-        [x_mn, y_mn, dist] = my_Lagrange_multiplier(eDiff(eqn_plot), ids, iqs);
+    for te=1:40
+        eqn = subs(eqn, Te, te);
+        [x_mn, y_mn, dist] = my_Lagrange_multiplier(eDiff(eqn), ids, iqs);
         MTPA_position(te,:) = [x_mn y_mn];
+        
+        wrm = subs(w_solution, [ids iqs], [x_mn y_mn]);
+        wrm = wrm(wrm>0);
+        wrm = double(wrm) / (P/2);
+        Pout(te) = wrm * te;
     end
 end
 
 
+figure;
+plot(1:40, Pout, 'k');
+xlabel("T_e [Nm]"); ylabel("P_{out} [W]")
+title("MTPA power");
+
+
 figure; hold on;
-f1=fimplicit(w_plot1, 'k--');
-f2=fimplicit(w_plot2, 'k--');
-f3=fimplicit(w_plot3, 'k--');
+plot(sol_ids, sol_iqs, 'ro');
+f1=fimplicit(w_eqn1, 'k--');
+f2=fimplicit(w_eqn2, 'k--');
+f3=fimplicit(w_eqn3, 'k--');
 f4=plot(-Lamf/Lds, 0, 'ro', displayname='전압제한타원 원점');
 drawnow;
 
-text(f1.XData(round(end/2)), f1.YData(round(end/2)),'w_{1}', 'backgroundColor', 'white', fontsize=12);
-text(f2.XData(round(end/2)), f2.YData(round(end/2)),'w_{2}', 'backgroundColor', 'white', fontsize=12);
-text(f3.XData(round(end/2)), f3.YData(round(end/2)),'w_{3}', 'backgroundColor', 'white', fontsize=12);
+text(f1.XData(1), f1.YData(1),'w_{1}', 'backgroundColor', 'white', fontsize=12);
+text(f2.XData(1), f2.YData(1),'w_{2}', 'backgroundColor', 'white', fontsize=12);
+text(f3.XData(1), f3.YData(1),'w_{3}', 'backgroundColor', 'white', fontsize=12);
 
 xlabel('d-axis'); ylabel('q-axis')
-axis([-Ismax 2 -Ismax Ismax]*1.5)
+axis([-Ismax 2 -6 6]*1.5)
 pbaspect([1 1 1])
 plot([0 0], [-Ismax Ismax] *1.5, 'k', 'LineWidth', 0.5);
 plot([-Ismax Ismax]*1.5, [0 0], 'k', 'LineWidth', 0.2);
-% hold off;
-% 
-% %%
-% figure; hold on;
 
-f1=fimplicit(Te_plot1, color='k');
-f2=fimplicit(Te_plot2, color='k');
-f3=fimplicit(Te_plot3, color='k');
-% f4=fimplicit(ids^2+iqs^2==dist_1^2, color='k');
-f5=fimplicit(ids^2+iqs^2==dist_2^2, color='k');
-f6=fimplicit(ids^2+iqs^2==dist_3^2, color='k');
-f7=plot(MTPA_position(:,1), MTPA_position(:,2), linewidth=2, color='b');
+ 
+% figure; hold on;
+f1te=fimplicit(Te_eqn1, color='k');
+f2te=fimplicit(Te_eqn2, color='k');
+f3te=fimplicit(Te_eqn3, color='k');
+f4te=fimplicit(Te_eqn4, color='k');
+
+f2di=fimplicit(ids^2+iqs^2==dist_2^2, color='k');
+f3di=fimplicit(ids^2+iqs^2==dist_3^2, color='k');
+f4di=fimplicit(ids^2+iqs^2==dist_4^2, color='k');
+f1MTPA=plot(MTPA_position(:,1), MTPA_position(:,2), linewidth=2, color='b');
 drawnow;
 
-text(f1.XData(end-3), f1.YData(end-3),'Te=10Nm', 'backgroundColor', 'white', fontsize=12);
-text(f2.XData(end-6), f2.YData(end-6),'Te=20Nm', 'backgroundColor', 'white', fontsize=12);
-text(f3.XData(end-9), f3.YData(end-9),'Te=30Nm', 'backgroundColor', 'white', fontsize=12);
-text(MTPA_position(end,1), MTPA_position(end,2), 'MTPA: ~100Nm', ...
+text(f1te.XData(end-3), f1te.YData(end-3),'Te=10Nm', 'backgroundColor', 'white', fontsize=12);
+text(f2te.XData(end-6), f2te.YData(end-6),'Te=20Nm', 'backgroundColor', 'white', fontsize=12);
+text(f3te.XData(end-9), f3te.YData(end-9),'Te=30Nm', 'backgroundColor', 'white', fontsize=12);
+text(f4te.XData(end-12),f4te.YData(end-12),'Te=40Nm', 'backgroundColor', 'white', fontsize=12);
+text(MTPA_position(end,1), MTPA_position(end,2), 'MTPA: ~40Nm', ...
     backgroundcolor='white', ...
     fontsize=12, color='b');
 
@@ -99,15 +125,13 @@ pbaspect([1 1 1])
 plot([0 0], [-Ismax Ismax] *1.5, 'k', 'LineWidth', 0.5);
 plot([-Ismax Ismax]*1.5, [0 0], 'k', 'LineWidth', 0.2); 
 
-%%
-subplot(7, 2, [13 14]); axis off;
 
 param_str = sprintf(...
-    'Lds = %.2f mH, Lqs = %.2f mH\n Lamf = %.1f Wb, P = %d, Vsmax = %.0f V', ...
-    0.03e-3*1e6, 0.04e-3*1e6, 1, 16, 40);
+    'Lds = %.2f mH, Lqs = %.2f mH\nLamf = %.2f Wb, P = %d, Vsmax = %.0f V', ...
+    Lds*1e3, Lqs*1e3, Lamf, P, Vsmax);
 
-text(0.5, 0.5, param_str, ...
-    'HorizontalAlignment', 'right', ...
+text(-14, -8, param_str, ...
+    'HorizontalAlignment', 'left', ...
     'VerticalAlignment', 'middle', ...
     'FontSize', 10, ...
     'Interpreter', 'none');
