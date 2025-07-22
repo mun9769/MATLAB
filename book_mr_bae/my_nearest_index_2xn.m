7ba6195d663a6ee61a54693929c2dc5832e501e2
moon <mun9769@naver.com>
2025-07-22 13:00:11 +0900

    refac

--- book_mr_bae/my_nearest_index_2xn.m
+++
@@ -1,9 +0,0 @@
-function idx = my_nearest_index_2xn(fxy, p0)
-% p0 = [1 2]
-% f = 2xn
-
-diff = fxy - p0;
-norms = vecnorm(diff, 2, 1);  % Compute L2 norm along each column (result: 1×100)
-[~,idx] = min(norms);
-
-end
