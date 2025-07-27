7e499d3e4147a50f2dc9e7f0eefa55cd59ebf2d7
moon <mun9769@naver.com>
2025-07-27 23:34:02 +0900

    gg

--- 抗力/Ex 2-1/A_Ex21.m
+++ 抗力/Ex 2-1/A_Ex21.m
@@ -14,10 +14,11 @@ L=1e-3;
 
 Vdc = 310;
 
-fsw=2.5e3;       % Switching frequency
+fsw=2500;       % Switching frequency
 Tsw=1/fsw;      % Switching period
 
 fs=2*fsw;       % Samping frequency (double sampling)
+fs=5000;       % Samping frequency (double sampling)
 Ts=1/fs;        % Samping period
 
 %% Command setting
@@ -30,4 +31,29 @@ W=2*pi*200;          % Angular speed of output voltage
 
 %% Run Simulink
 
-sim('Sim_Ex21.slx')
+    out=sim('Sim_Ex21.slx');
+
+
+
+test =       (logsout.get("test").Values.Data );
+tt = logsout.get("test").Values.Time;
+
+Sabc =       (logsout.get("Sabc").Values.Data );
+ta = logsout.get("Sabc").Values.Time;
+
+test_interpolate = interp1(tt,test, ta, 'previous');
+
+test_interpolate( isnan(test_interpolate) ) = 0;
+
+% plot(ta, test_interpolate);
+
+
+li = zeros(size(Sabc));
+for ii=1:length(Sabc)
+
+    li(ii) = Sabc(ii,1)*4 + Sabc(ii,2)*2 + Sabc(ii,1);
+end
+
+plot(ta, li);
+
+
