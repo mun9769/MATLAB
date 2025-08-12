33e3068cdcfeb46c7bffcdcdf4e44d3476ba9b76
moon <mun9769@naver.com>
2025-08-12 13:51:20 +0900

    eee

--- Electrical_Machines_Control_problems/animation_dq_to_abc.m
+++ Electrical_Machines_Control_problems/animation_dq_to_abc.m
@@ -13,21 +13,7 @@ fig = figure;
 
 [p11, ax1] = my_set_ax1(fig);
 % [p22] = my_set_ax2(fig);
-[p1, p2, p3] = my_set_ax3(fig);
-
-
-theta = linspace(0,4*pi, 629);
-idx = 1;
-for ii=theta
-    VV = TT(ii) \ [Vd Vq 0]';
-    V_abc(idx,:) = VV;
-    idx = idx + 1;
-end
-
-p1.XData = theta; p2.XData = theta; p3.XData = theta;
-p1.YData = V_abc(:,1);
-p2.YData = V_abc(:,2);
-p3.YData = V_abc(:,3);
+[p1, p2, p3] = my_set_ax3_new(fig);
 
 r = 10/sqrt(3);
 rot = [cos(pi/2) -sin(pi/2); sin(pi/2) cos(pi/2)];
@@ -35,69 +21,46 @@ sf = 1.8;
 
 vidObj = VideoWriter('gogo', 'MPEG-4');
 open(vidObj);
+dtheta=1e-2;
+
 
 for t=linspace(-pi/2, pi/2, 10)
 
     Vd = r*cos(t);
     Vq = r*sin(t);
-    
     kkk = rot*[Vd Vq]';
+
 
     q=quiver(ax1, 0,0, Vd*sf, Vq*sf, 'k--');
     q2 = quiver(ax1, 0,0, kkk(1)*sf, kkk(2)*sf, 'k--');
+    cur = 0;
+    kk = linspace(0,10*pi, 100);
+    for zz = kk
+        aa = Vd*cos(zz);
+        bb = Vq*cos(zz);
 
-    set(p11, 'XData', Vd, 'YData', Vq);
-    idx = 1;
-    for ii=theta
-        VV = TT(ii) \ [Vd Vq 0]';
-        V_abc(idx,:) = VV;
-        idx = idx + 1;
-    end
-    p1.YData = V_abc(:,1);
-    p2.YData = V_abc(:,2);
-    p3.YData = V_abc(:,3);
-    for iii = 1:100
+        V_abc = TT(0) \ [aa bb 0]';
+        p1.XData(end+1) = cur;    p1.YData(end+1) = V_abc(1);
+        p2.XData(end+1) = cur;    p2.YData(end+1) = V_abc(2);
+        p3.XData(end+1) = cur;    p3.YData(end+1) = V_abc(3);
+        cur = cur + dtheta;
+
+        set(p11, 'XData', aa, 'YData', bb);
+
         currFrame = getframe(fig);
         writeVideo(vidObj, currFrame);
+
+        if zz == kk(end)
+            cur = 0;
+            p1.XData = []; p1.YData = [];
+            p2.XData = []; p2.YData = [];
+            p3.XData = []; p3.YData = [];
+        end
     end
     pause(0);
     delete(q);
     delete(q2);
 end
 close(vidObj);
-
-
-%%
-
-dtheta=1e-2;
-cur = 0;
-
-iter = 1000;
-for thetar=linspace(0, iter*2*pi, iter*629+1) % 2*pi에 628개
-
-    % V_abc = inv( TT(thetar) ) * [Vd Vq 0]';
-    V_abc = TT(thetar) \ [Vd Vq 0]';
-    
-    p1.XData(end+1) = cur;    p1.YData(end+1) = V_abc(1);
-    p2.XData(end+1) = cur;    p2.YData(end+1) = V_abc(2);
-    p3.XData(end+1) = cur;    p3.YData(end+1) = V_abc(3);
-    cur = cur + dtheta;
-
-    V_dqs_e = [cos(thetar) -sin(thetar);
-               sin(thetar)  cos(thetar)] * [Vd Vq]';
-    p22.XData = V_dqs_e(1);
-    p22.YData = V_dqs_e(2);
-
-
-    if(cur >= 8*pi)
-        cur = 0;
-        p1.XData = []; p1.YData = [];
-        p2.XData = []; p2.YData = [];
-        p3.XData = []; p3.YData = [];
-    end
-    % drawnow;
-    % saveas(gcf,['.\output\image' num2str(thetar) '.png']); % 사진으로 저장하기
-    pause(0)
-end
 
 
