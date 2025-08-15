eef432a7e484ec2fab0d67d4896d6e06fe707e63
moon <mun9769@naver.com>
2025-08-16 02:55:14 +0900

    eeee

--- try_again/main.m
+++ try_again/main.m
@@ -14,8 +14,8 @@ rad2deg = 180/pi;
 mm = 2; % 1. 속도+전류 제어 2. 전류제어
 Mode = 2; % 2: sensorless
 
-w_h = 2*pi*800;
-V_inj = 60;
+w_h = 2*pi*3000;
+V_inj = 10;
 Stop_Time = 10;
 J = 0.045;
 B=0.0001;
@@ -51,15 +51,41 @@ Kas=1/Kps;
 Vsmax = 60;
 
 
-init_Thetar = 3*pi/7;
+init_Thetar = 2*pi/7;
 
-Thetar_hat_init = 0;
+Thetar_hat_init = pi;
 
-zeta = 1.4;
+zeta = 0.8;
 %%
-G = tf([2*zeta*w_h 0], [1 2*zeta*w_h w_h^2]);
+out = sim('try_again_agin');
 
-margin(G);
+Theta_rhat   =  squeeze (out.logsout.get("Theta_rhat").Values.Data );
+Thetar_tilde =  squeeze( out.logsout.get("Thetar_tilde").Values.Data );
+
+tt = out.tout;
+
+figure; hold on;
+index = 1:20;
+plot(tt(index), Theta_rhat(index), 'ro');
+
+plot(tt(index), Thetar_tilde(index), 'bo');
 
 
-Stop_Time = 0.000001
+for ii=2:size(tt, 1)
+    assert(Theta_rhat(ii) == Theta_rhat(ii-1) + Thetar_tilde(ii), "hawing");
+    Theta_rhat(ii)
+end
+
+sum = 0;
+for ii=2:size(tt,1)
+    sum = sum + Thetar_tilde(ii);
+end
+
+sum
+
+%%
+G = tf([1 0], [1 w_h/10]);
+
+
+
+margin(G)
